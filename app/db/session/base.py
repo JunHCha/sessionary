aab807c70c76553b7008861f77d371ecf5179c62
junhcha <quffl9612@naver.com>
2023-07-30 22:03:26 +0900

    build: add timestamp columns to the table

--- app/db/session/base.py
+++ app/db/session/base.py
@@ -1,5 +1,7 @@
+from sqlalchemy import Column, DateTime, func
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    ...
+    time_created = Column(DateTime, default=func.now())
+    time_updated = Column(DateTime, default=func.now(), onupdate=func.now())
