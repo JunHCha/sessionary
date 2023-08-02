afd8dfc19d6d8bf9a0d6786167708f2af979d880
junhcha <quffl9612@naver.com>
2023-08-02 23:58:20 +0900

    feat: add auth database_strategy

--- app/db/session/depends.py
+++ app/db/session/depends.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 from fastapi import Depends
+from fastapi_users.authentication.strategy import AccessTokenDatabase, DatabaseStrategy
 from fastapi_users.db import SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
@@ -28,3 +29,9 @@ async def get_access_token_db(
     session: AsyncSession = Depends(get_async_session),
 ):
     yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(access_token_db, lifetime_seconds=3600 * 24 * 7 * 4)
