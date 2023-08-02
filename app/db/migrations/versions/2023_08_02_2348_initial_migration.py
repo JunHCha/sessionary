eb597daa145339f9be1c4bc20a56aba66612d038
junhcha <quffl9612@naver.com>
2023-08-02 23:52:31 +0900

    build: rewrite initial migration

--- app/db/migrations/versions/2023_08_02_2348_initial_migration.py
+++ app/db/migrations/versions/2023_08_02_2348_initial_migration.py
@@ -1,8 +1,8 @@
-"""Initial migrations
+"""initial migration
 
-Revision ID: 9993ad4809e6
+Revision ID: 9a7be4df1534
 Revises: 
-Create Date: 2023-07-31 17:59:40.489730
+Create Date: 2023-08-02 23:48:52.380805
 
 """
 import fastapi_users_db_sqlalchemy
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "9993ad4809e6"
+revision = "9a7be4df1534"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,14 +21,14 @@ def upgrade() -> None:
     op.create_table(
         "artist",
         sa.Column("nickname", sa.String(), nullable=False),
+        sa.Column("time_created", sa.DateTime(), nullable=True),
+        sa.Column("time_updated", sa.DateTime(), nullable=True),
         sa.Column("id", fastapi_users_db_sqlalchemy.generics.GUID(), nullable=False),
         sa.Column("email", sa.String(length=320), nullable=False),
         sa.Column("hashed_password", sa.String(length=1024), nullable=False),
         sa.Column("is_active", sa.Boolean(), nullable=False),
         sa.Column("is_superuser", sa.Boolean(), nullable=False),
         sa.Column("is_verified", sa.Boolean(), nullable=False),
-        sa.Column("time_created", sa.DateTime(), nullable=True),
-        sa.Column("time_updated", sa.DateTime(), nullable=True),
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("nickname"),
     )
@@ -52,7 +52,7 @@ def upgrade() -> None:
             nullable=True,
         ),
         sa.Column("is_active", sa.Boolean(), nullable=False),
-        sa.Column("expire_at", sa.DateTime(), nullable=False),
+        sa.Column("expires_at", sa.DateTime(), nullable=False),
         sa.Column("time_created", sa.DateTime(), nullable=True),
         sa.Column("time_updated", sa.DateTime(), nullable=True),
         sa.PrimaryKeyConstraint("id"),
@@ -60,18 +60,35 @@ def upgrade() -> None:
     op.create_table(
         "user",
         sa.Column("nickname", sa.String(), nullable=False),
+        sa.Column("time_created", sa.DateTime(), nullable=True),
+        sa.Column("time_updated", sa.DateTime(), nullable=True),
         sa.Column("id", fastapi_users_db_sqlalchemy.generics.GUID(), nullable=False),
         sa.Column("email", sa.String(length=320), nullable=False),
         sa.Column("hashed_password", sa.String(length=1024), nullable=False),
         sa.Column("is_active", sa.Boolean(), nullable=False),
         sa.Column("is_superuser", sa.Boolean(), nullable=False),
         sa.Column("is_verified", sa.Boolean(), nullable=False),
-        sa.Column("time_created", sa.DateTime(), nullable=True),
-        sa.Column("time_updated", sa.DateTime(), nullable=True),
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("nickname"),
     )
     op.create_index(op.f("ix_user_email"), "user", ["email"], unique=True)
+    op.create_table(
+        "accesstoken",
+        sa.Column(
+            "user_id", fastapi_users_db_sqlalchemy.generics.GUID(), nullable=False
+        ),
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(["user_id"], ["user.id"], ondelete="cascade"),
+        sa.PrimaryKeyConstraint("token"),
+    )
+    op.create_index(
+        op.f("ix_accesstoken_created_at"), "accesstoken", ["created_at"], unique=False
+    )
     op.create_table(
         "lecture",
         sa.Column("id", sa.Integer(), nullable=False),
@@ -99,8 +116,6 @@ def upgrade() -> None:
         sa.Column("refresh_token", sa.String(length=1024), nullable=True),
         sa.Column("account_id", sa.String(length=320), nullable=False),
         sa.Column("account_email", sa.String(length=320), nullable=False),
-        sa.Column("time_created", sa.DateTime(), nullable=True),
-        sa.Column("time_updated", sa.DateTime(), nullable=True),
         sa.ForeignKeyConstraint(["user_id"], ["user.id"], ondelete="cascade"),
         sa.PrimaryKeyConstraint("id"),
     )
@@ -125,6 +140,8 @@ def downgrade() -> None:
     op.drop_index(op.f("ix_oauth_account_account_id"), table_name="oauth_account")
     op.drop_table("oauth_account")
     op.drop_table("lecture")
+    op.drop_index(op.f("ix_accesstoken_created_at"), table_name="accesstoken")
+    op.drop_table("accesstoken")
     op.drop_index(op.f("ix_user_email"), table_name="user")
     op.drop_table("user")
     op.drop_table("subscription")
