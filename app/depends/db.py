492499a9894cb6cf4f148b87f2cc665bf8c5e1ee
junhcha <quffl9612@naver.com>
2023-08-03 01:01:26 +0900

    fix: use postgresql+asyncpg scheme for better experience in using sqlalchemy

--- app/depends/db.py
+++ app/depends/db.py
@@ -4,8 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from app.core.config import get_app_settings
 
+
+def get_asyncpg_url(database_url: str) -> str:
+    return database_url.replace("postgresql://", "postgresql+asyncpg://")
+
+
 SETTINGS = get_app_settings()
-DATABASE_URL = SETTINGS.database_url
+DATABASE_URL = get_asyncpg_url(SETTINGS.database_url)
+
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
