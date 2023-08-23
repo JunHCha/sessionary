e4062a442cd6ce28083665a550560475f5fa3978
junhcha <quffl9612@naver.com>
2023-08-23 23:15:03 +0900

    refactor: fix api router file structure

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from . import auth, ping
+from app.api.routes import user
 
 api_router = APIRouter()
-api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(user.app_router, prefix="/user", tags=["user"])
