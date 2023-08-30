4c0178450df164cd86dcbbafd0dc3c9d64510663
junhcha <quffl9612@naver.com>
2023-08-30 21:38:36 +0900

    build: add playlist and relation tables

--- app/db/tables.py
+++ app/db/tables.py
@@ -37,7 +37,6 @@ def random_nickname():
 class User(SQLAlchemyBaseUserTableUUID, Base):
     nickname = Column(String, default=random_nickname, unique=True, nullable=False)
     is_artist = Column(Boolean, default=False, nullable=False)
-    is_admin = Column(Boolean, default=False, nullable=False)
     subscription_id = Column(Integer, ForeignKey("subscription.id"), nullable=True)
     time_created = Column(DateTime, default=func.now())
     time_updated = Column(DateTime, default=func.now(), onupdate=func.now())
@@ -48,8 +47,12 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     )
     subscription: Mapped["Subscription"] = relationship("Subscription")
     lectures: Mapped[List["Lecture"]] = relationship(
-        "Lecture", back_populates="artist", lazy="joined"
+        "Lecture", secondary="artist_x_lecture", back_populates="artists"
     )
+    playlists: Mapped[List["Playlist"]] = relationship(
+        "Playlist", back_populates="owner"
+    )
+    lessons: Mapped[List["Lesson"]] = relationship("Lesson", back_populates="artist")
 
 
 class Subscription(Base):
@@ -73,40 +76,14 @@ class Lecture(Base):
     time_updated = Column(DateTime, default=func.now(), onupdate=func.now())
 
     # for orm
+    artists: Mapped[List[User]] = relationship(
+        "User", secondary="artist_x_lecture", back_populates="lectures"
+    )
     lessons: Mapped[List["Lesson"]] = relationship(
-        "Lesson", secondary="lecture_x_lesson", back_populates="lecture"
+        "Lesson", secondary="lecture_x_lesson", back_populates="lectures"
     )
 
     __tablename__ = "lecture"
-
-
-class Lesson(Base):
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    artist_id = Column(UUID, ForeignKey("user.id"), nullable=False)
-    sheetmusic_url = Column(String)
-    video_url = Column(String)
-    text = Column(String)
-    time_created = Column(DateTime, default=func.now())
-    time_updated = Column(DateTime, default=func.now(), onupdate=func.now())
-
-    # for orm
-    lectures = relationship(
-        "Lecture", secondary="lecture_x_lesson", back_populates="lessons"
-    )
-    playlists = relationship(
-        "Playlist", secondary="playlist_x_lesson", back_populates="lessons"
-    )
-    artist = relationship("User", back_populates="lectures")
-
-    __tablename__ = "lesson"
-
-
-class LectureXLesson(Base):
-    lecture_id = Column(Integer, ForeignKey("lecture.id"), primary_key=True)
-    lesson_id = Column(Integer, ForeignKey("lesson.id"), primary_key=True)
-
-    __tablename__ = "lecture_x_lesson"
 
 
 class Playlist(Base):
@@ -120,11 +97,47 @@ class Playlist(Base):
 
     # for orm
     owner: Mapped[User] = relationship("User", back_populates="playlists")
-    lessons = relationship(
+    lessons: Mapped[List["Lesson"]] = relationship(
         "Lesson", secondary="playlist_x_lesson", back_populates="playlists"
     )
 
     __tablename__ = "playlist"
+
+
+class Lesson(Base):
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    artist_id = Column(UUID, ForeignKey("user.id"), nullable=False)
+    sheetmusic_url = Column(String)
+    video_url = Column(String)
+    text = Column(String)
+    time_created = Column(DateTime, default=func.now())
+    time_updated = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    # for orm
+    lectures: Mapped[List[Lecture]] = relationship(
+        "Lecture", secondary="lecture_x_lesson", back_populates="lessons"
+    )
+    playlists: Mapped[List[Playlist]] = relationship(
+        "Playlist", secondary="playlist_x_lesson", back_populates="lessons"
+    )
+    artist: Mapped[User] = relationship("User", back_populates="lessons")
+
+    __tablename__ = "lesson"
+
+
+class ArtistXLecture(Base):
+    artist_id = Column(UUID, ForeignKey("user.id"), primary_key=True)
+    lecture_id = Column(Integer, ForeignKey("lecture.id"), primary_key=True)
+
+    __tablename__ = "artist_x_lecture"
+
+
+class LectureXLesson(Base):
+    lecture_id = Column(Integer, ForeignKey("lecture.id"), primary_key=True)
+    lesson_id = Column(Integer, ForeignKey("lesson.id"), primary_key=True)
+
+    __tablename__ = "lecture_x_lesson"
 
 
 class PlaylistXLesson(Base):
