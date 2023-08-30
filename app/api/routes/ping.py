1c48f0ec918dec5c085e9149568c903d0d128c36
junhcha <quffl9612@naver.com>
2023-08-30 21:44:22 +0900

    feat: add auth test api

--- app/api/routes/ping.py
+++ app/api/routes/ping.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.core.auth.backend import current_active_user
+from app.db.tables import User
 
 app_router = APIRouter()
 
@@ -6,3 +9,8 @@ app_router = APIRouter()
 @app_router.get("")
 async def pong():
     return {"ping": "pong!"}
+
+
+@app_router.get("/auth")
+async def auth_pong(user: User = Depends(current_active_user)):
+    return {"ping": "pong!", "data": user}
