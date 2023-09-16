b8ad4c52ea16771e72217fab2236351080c5f66b
junhcha <quffl9612@naver.com>
2023-09-16 16:22:49 +0900

    chore: remove comment

--- app/api/routes/ping.py
+++ app/api/routes/ping.py
@@ -13,5 +13,4 @@ async def pong():
 
 @app_router.get("/auth")
 async def auth_pong(user: User = Depends(get_current_user)):
-    # async def auth_pong(user: User = Depends(current_user)):
     return {"ping": "pong!", "data": user}
