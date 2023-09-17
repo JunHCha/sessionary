0dc363b3edd8bffbdb8595563021d567fd40c874
junhcha <quffl9612@naver.com>
2023-09-17 21:11:49 +0900

    refactor: unwrap Depends for app settings

--- app/depends/db.py
+++ app/depends/db.py
@@ -10,10 +10,8 @@ from app.db.session import SessionManager
 from app.db.tables import AccessToken, OAuthAccount, User
 
 
-async def get_session(
-    settings=Depends(get_app_settings),
-) -> AsyncGenerator[AsyncSession, None]:
-    session_manager = SessionManager(settings)
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    session_manager = SessionManager(settings=get_app_settings())
     async with session_manager.async_session() as async_session:
         yield async_session
 
