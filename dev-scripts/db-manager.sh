0aaf95ada7a54135431eb5e0e2d585f26d31dce6
junhcha <quffl9612@naver.com>
2024-04-12 01:51:23 +0900

    build: fix db-manager env var

--- dev-scripts/db-manager.sh
+++ dev-scripts/db-manager.sh
@@ -3,16 +3,15 @@
 set +ex
 
 ARGS="$@"
-DATABASE_URL=postgresql://user:password@127.0.0.1:5432/sessionaway
-SECRET_KEY=TEMP
+APP_ENV=dev
 
 if [[ "$ARGS" == "upgrade" ]]; then
-    DATABASE_URL=$DATABASE_URL SECRET_KEY=$SECRET_KEY alembic upgrade head
+    APP_ENV=dev alembic upgrade head
 elif [[ "$ARGS" == "migration "* ]]; then
     ARGS="${ARGS#migration }" # remove "migration " prefix
-    DATABASE_URL=$DATABASE_URL SECRET_KEY=$SECRET_KEY alembic revision --autogenerate -m "$ARGS"
+    APP_ENV=dev alembic revision --autogenerate -m "$ARGS"
 elif [[ "$ARGS" == "downgrade" ]]; then
-    DATABASE_URL=$DATABASE_URL SECRET_KEY=$SECRET_KEY alembic downgrade -1
+    APP_ENV=dev alembic downgrade -1
 else
     echo "Usage: $0 [upgrade|migration <message>|downgrade]"
     exit 1
