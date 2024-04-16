6f062846431f564f6523acb074f0e703a2518e72
junhcha <quffl9612@naver.com>
2024-04-16 21:49:41 +0900

    refactor: slide custiom api endpoint

--- app/user/api/__init__.py
+++ app/user/api/__init__.py
@@ -7,13 +7,13 @@ from app.user.service import BaseUserService, get_user_service
 app_router = APIRouter()
 
 
-app_router.include_router(auth_backend.authenticate_router, prefix="/auth")
-app_router.include_router(auth_backend.password_reset_router, prefix="/auth")
-app_router.include_router(auth_backend.oauth_router, prefix="/oauth/google")
-app_router.include_router(auth_backend.users_router)
-
-
 @app_router.get("/artists", response_model=GetArtistsResponse)
 async def get_artists(user_svc: BaseUserService = Depends(get_user_service)):
     results = await user_svc.get_artists()
     return GetArtistsResponse(data=results)
+
+
+app_router.include_router(auth_backend.authenticate_router, prefix="/auth")
+app_router.include_router(auth_backend.password_reset_router, prefix="/auth")
+app_router.include_router(auth_backend.oauth_router, prefix="/oauth/google")
+app_router.include_router(auth_backend.users_router)
