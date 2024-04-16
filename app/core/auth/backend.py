1415bb09f63e862f91e78d8ab9a426a5c631ddd1
junhcha <quffl9612@naver.com>
2024-04-16 21:42:36 +0900

    refactor: include user routers to backend object

--- app/core/auth/backend.py
+++ app/core/auth/backend.py
@@ -1,7 +1,7 @@
 import uuid
-from typing import Any
 
 import redis
+from fastapi import APIRouter
 from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import (
     AuthenticationBackend,
@@ -9,13 +9,13 @@ from fastapi_users.authentication import (
     CookieTransport,
 )
 from fastapi_users.authentication.strategy import RedisStrategy
-from fastapi_users.models import UserProtocol
 from httpx_oauth.clients.google import GoogleOAuth2
 
 from app.core.auth.dependancy import get_user_manager
 from app.core.settings import get_app_settings
 from app.core.settings.base import AppSettings
 from app.db.tables import User
+from app.user.api.schemas import UserRead, UserUpdate
 
 
 class AuthBackend:
@@ -30,24 +30,42 @@ class AuthBackend:
         self.google_client_id = settings.google_client_id
         self.google_client_secret = settings.google_client_secret
         self.auth_redis_url = settings.auth_redis_url
-
-    @property
-    def backend(self) -> FastAPIUsers:
-        return FastAPIUsers[User, uuid.UUID](get_user_manager, [self.auth_backend])
-
-    @property
-    def auth_backend(self) -> AuthenticationBackend[UserProtocol, Any]:
-        return AuthenticationBackend(
+        self.auth_backend = AuthenticationBackend(
             name="redis",
             transport=self.bearer_transport,
-            get_strategy=self._get_redis_strategy,
+            get_strategy=self.get_redis_strategy,
+        )
+        self.components = FastAPIUsers[User, uuid.UUID](
+            get_user_manager, [self.auth_backend]
+        )
+        self.google_oauth_client = GoogleOAuth2(
+            self.google_client_id, self.google_client_secret
         )
 
     @property
-    def google_oauth_client(self) -> GoogleOAuth2:
-        return GoogleOAuth2(self.google_client_id, self.google_client_secret)
+    def authenticate_router(self) -> APIRouter:
+        return self.components.get_auth_router(self.auth_backend)
 
-    def _get_redis_strategy(self) -> RedisStrategy:
+    @property
+    def oauth_router(self) -> APIRouter:
+        return self.components.get_oauth_router(
+            auth_backend.google_oauth_client,
+            auth_backend.auth_backend,
+            state_secret="SECRET",  # TODO: 정확한 사용법을 확인 후 수정
+            associate_by_email=True,
+        )
+
+    @property
+    def password_reset_router(self) -> APIRouter:
+        return self.components.get_reset_password_router()
+
+    @property
+    def users_router(self) -> APIRouter:
+        return self.components.get_users_router(
+            user_schema=UserRead, user_update_schema=UserUpdate
+        )
+
+    def get_redis_strategy(self) -> RedisStrategy:
         redis_client = redis.asyncio.from_url(
             self.auth_redis_url, decode_responses=True
         )
@@ -59,4 +77,3 @@ class AuthBackend:
 
 
 auth_backend = AuthBackend(settings=get_app_settings())
-fastapi_users_component = auth_backend.backend
--- app/user/api/__init__.py
+++ app/user/api/__init__.py
@@ -1,34 +1,19 @@
 from fastapi import APIRouter, Depends
 
-from app.core.auth.backend import auth_backend, fastapi_users_component
-from app.user.api.schemas import GetArtistsResponse, UserRead, UserUpdate
+from app.core.auth.backend import auth_backend
+from app.user.api.schemas import GetArtistsResponse
 from app.user.service import BaseUserService, get_user_service
 
 app_router = APIRouter()
+
+
+app_router.include_router(auth_backend.authenticate_router, prefix="/auth")
+app_router.include_router(auth_backend.password_reset_router, prefix="/auth")
+app_router.include_router(auth_backend.oauth_router, prefix="/oauth/google")
+app_router.include_router(auth_backend.users_router)
 
 
 @app_router.get("/artists", response_model=GetArtistsResponse)
 async def get_artists(user_svc: BaseUserService = Depends(get_user_service)):
     results = await user_svc.get_artists()
     return GetArtistsResponse(data=results)
-
-
-app_router.include_router(
-    fastapi_users_component.get_auth_router(auth_backend.auth_backend), prefix="/auth"
-)
-
-app_router.include_router(
-    fastapi_users_component.get_oauth_router(
-        auth_backend.google_oauth_client,
-        auth_backend.auth_backend,
-        state_secret="SECRET",  # TODO: 정확한 사용법을 확인 후 수정
-        associate_by_email=True,
-    ),
-    prefix="/oauth/google",
-)
-app_router.include_router(fastapi_users_component.get_reset_password_router())
-app_router.include_router(
-    fastapi_users_component.get_users_router(
-        user_schema=UserRead, user_update_schema=UserUpdate
-    )
-)
