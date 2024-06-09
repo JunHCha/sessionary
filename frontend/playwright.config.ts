f9b19a8d02d1ba94c83717629813d3f8b0341b1b
junhcha <quffl9612@naver.com>
2024-06-09 15:24:53 +0900

    test: add webserver url

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -70,6 +70,7 @@ export default defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run dev',
+		url: 'http://localhost:5173',
 		reuseExistingServer: !process.env.CI,
 		timeout: 120 * 1000
 	}
