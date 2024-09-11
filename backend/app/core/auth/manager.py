27cc7fd54721941e9a098fd4d464529cc334b306
junhcha <quffl9612@naver.com>
2024-09-12 01:35:40 +0900

    refactor: refresh user instance at on_after_register hook

--- backend/app/core/auth/manager.py
+++ backend/app/core/auth/manager.py
@@ -3,157 +3,21 @@ import uuid
 from typing import Optional
 
 from fastapi import Request
-from fastapi_users import BaseUserManager, UUIDIDMixin, exceptions, models, schemas
-from sqlalchemy import insert
+from fastapi_users import BaseUserManager, UUIDIDMixin
+from sqlalchemy import insert, update
 
 from app.db.tables import Subscription, User, UserXSubscription
-from app.depends.db import get_session
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = "SECRET"
-
-    async def create(
-        self,
-        user_create: schemas.UC,
-        safe: bool = False,
-        request: Request | None = None,
-    ) -> models.UP:
-        """
-        Create a user in database.
-
-        Triggers the on_after_register handler on success.
-
-        :param user_create: The UserCreate model to create.
-        :param safe: If True, sensitive values like is_superuser or is_verified
-        will be ignored during the creation, defaults to False.
-        :param request: Optional FastAPI request that
-        triggered the operation, defaults to None.
-        :raises UserAlreadyExists: A user already exists with the same e-mail.
-        :return: A new user.
-        """
-        await self.validate_password(user_create.password, user_create)
-
-        existing_user = await self.user_db.get_by_email(user_create.email)
-        if existing_user is not None:
-            raise exceptions.UserAlreadyExists()
-
-        user_dict = (
-            user_create.create_update_dict()
-            if safe
-            else user_create.create_update_dict_superuser()
-        )
-        password = user_dict.pop("password")
-        user_dict["hashed_password"] = self.password_helper.hash(password)
-
-        created_user = await self.user_db.create(user_dict)
-
-        await self.on_after_register(created_user, request)
-
-        # query again to get the full user with subscriptions
-        # TODO: This is a workaround to get the full user with subscriptions.
-        #       This should be fixed in the future.
-        async for sess in get_session():
-            user = await sess.get(User, created_user.id)
-
-        return user
-
-    async def oauth_callback(
-        self: "BaseUserManager[models.UOAP, models.ID]",
-        oauth_name: str,
-        access_token: str,
-        account_id: str,
-        account_email: str,
-        expires_at: Optional[int] = None,
-        refresh_token: Optional[str] = None,
-        request: Optional[Request] = None,
-        *,
-        associate_by_email: bool = False,
-        is_verified_by_default: bool = False,
-    ) -> models.UOAP:
-        """
-        Handle the callback after a successful OAuth authentication.
-
-        If the user already exists with this OAuth account, the token is updated.
-
-        If a user with the same e-mail already exists and `associate_by_email` is True,
-        the OAuth account is associated to this user.
-        Otherwise, the `UserNotExists` exception is raised.
-
-        If the user does not exist, it is created and the on_after_register handler
-        is triggered.
-
-        :param oauth_name: Name of the OAuth client.
-        :param access_token: Valid access token for the service provider.
-        :param account_id: models.ID of the user on the service provider.
-        :param account_email: E-mail of the user on the service provider.
-        :param expires_at: Optional timestamp at which the access token expires.
-        :param refresh_token: Optional refresh token to get a
-        fresh access token from the service provider.
-        :param request: Optional FastAPI request that
-        triggered the operation, defaults to None
-        :param associate_by_email: If True, any existing user with the same
-        e-mail address will be associated to this user. Defaults to False.
-        :param is_verified_by_default: If True, the `is_verified` flag will be
-        set to `True` on newly created user. Make sure the OAuth Provider you're
-        using does verify the email address before enabling this flag.
-        Defaults to False.
-        :return: A user.
-        """
-        oauth_account_dict = {
-            "oauth_name": oauth_name,
-            "access_token": access_token,
-            "account_id": account_id,
-            "account_email": account_email,
-            "expires_at": expires_at,
-            "refresh_token": refresh_token,
-        }
-
-        try:
-            user = await self.get_by_oauth_account(oauth_name, account_id)
-        except exceptions.UserNotExists:
-            try:
-                # Associate account
-                user = await self.get_by_email(account_email)
-                if not associate_by_email:
-                    raise exceptions.UserAlreadyExists()
-                user = await self.user_db.add_oauth_account(user, oauth_account_dict)
-            except exceptions.UserNotExists:
-                # Create account
-                password = self.password_helper.generate()
-                user_dict = {
-                    "email": account_email,
-                    "hashed_password": self.password_helper.hash(password),
-                    "is_verified": is_verified_by_default,
-                }
-                user = await self.user_db.create(user_dict)
-                user = await self.user_db.add_oauth_account(user, oauth_account_dict)
-                await self.on_after_register(user, request)
-
-            # query again to get the full user with subscriptions
-            # TODO: This is a workaround to get the full user with subscriptions.
-            #       This should be fixed in the future.
-            async for sess in get_session():
-                user = await sess.get(User, user.id)
-        else:
-            # Update oauth
-            for existing_oauth_account in user.oauth_accounts:
-                if (
-                    existing_oauth_account.account_id == account_id
-                    and existing_oauth_account.oauth_name == oauth_name
-                ):
-                    user = await self.user_db.update_oauth_account(
-                        user, existing_oauth_account, oauth_account_dict
-                    )
-
-        return user
 
     async def on_after_register(
         self, user: User, request: Request | None = None
     ) -> None:
         """task: Automatically subscribe user to ticket plan after registration."""
 
-        async for sess in get_session():
+        async with self.user_db.session as sess:
             insert_subscription_stmt = (
                 insert(Subscription)
                 .values(
@@ -165,11 +29,21 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
                 .returning(Subscription)
             )
             subscription = (await sess.execute(insert_subscription_stmt)).scalar_one()
+
             insert_association_stmt = insert(UserXSubscription).values(
                 user_id=user.id, subscription_id=subscription.id
             )
             await sess.execute(insert_association_stmt)
+
+            update_user_subscription_stmt = (
+                update(User)
+                .where(User.id == user.id)
+                .values(subscription_id=subscription.id)
+            )
+
+            await sess.execute(update_user_subscription_stmt)
             await sess.commit()
+            await sess.refresh(user, ["subscription"])
 
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional[Request] = None
