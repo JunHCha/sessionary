b90b91391a5a89dcc5b901cde89740df3a81c7f8
JunHyung Cha <quffl9612@naver.com>
2024-09-12 16:11:51 +0900

    ⚙️🧪 remove backend test codes' dependancies on Docker container (#19)
    * test: remove deps on test db
    
    * chore: cleanup import
    
    * fix: simplify subscription fetch query
    
    * refactor: refresh user instance at on_after_register hook
    
    * test: do not depends on redis docker container
    
    * fix: use file db to handle fkey constraints
    
    * perf: reduce query
    
    * chore: fix gitignore
    
    * fix: apply uuid explicit
    
    * chore: remove not mendatory option
    
    * ci: fix fe-test gh action name
    
    * ci: add be-test gh action
    
    * ci: remove code review job name
    
    * fix: remove invalid parameter
    
    * fix: name the step
    
    * test: add workdir to python path
    
    * build: update packages
    
    * ci: fix triggers
    
    * ci: remove venv cache step
    
    * test: remove unnecesarry db setup

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,27 +1,28 @@
 import asyncio
 from os import environ
-from typing import AsyncGenerator, Iterator
+from typing import AsyncGenerator
 
 import pytest
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordRequestForm
-from redis.asyncio import Redis
-from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy_utils import create_database, database_exists, drop_database
+from fastapi_users import exceptions
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
 
+from app.core.auth.manager import UserManager
+from app.core.settings.base import AppSettings
+from app.db import tables
 from app.db.session import SessionManager
+from app.depends.auth import get_user_db, get_user_manager
+from app.depends.db import get_session
 from app.depends.settings import get_app_settings
-
-
-@pytest.fixture(scope="session")
-def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-
-    yield loop
-
-    loop.close()
+from tests.mock.redis_mock import RedisMock
 
 
 @pytest.fixture(scope="session")
@@ -34,37 +35,45 @@ def setup_env():
 @pytest.fixture(scope="session")
 def stub_sess_manager(setup_env) -> SessionManager:
     settings = get_app_settings()
-    return SessionManager(settings)
+
+    class TestSessionManager(SessionManager):
+        def __init__(self, settings: AppSettings):
+            self._engine = create_async_engine(
+                settings.database_url,
+                echo=True,
+            )
+            self._async_session_factory = async_scoped_session(
+                async_sessionmaker(
+                    self._engine,
+                    expire_on_commit=False,
+                    autoflush=False,
+                ),
+                scopefunc=asyncio.current_task,
+            )
+
+    return TestSessionManager(settings)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def setup_test_database(stub_sess_manager):
-
-    sync_engine_url = stub_sess_manager.engine.url.set(drivername="postgresql")
-
-    if database_exists(sync_engine_url):
-        drop_database(sync_engine_url)
-
-    create_database(sync_engine_url)
-
+async def setup_test_database(stub_sess_manager):
+    engine = stub_sess_manager.engine
+    async with engine.begin() as conn:
+        await conn.run_sync(tables.Base.metadata.create_all)
     yield
-
-    drop_database(sync_engine_url)
+    async with engine.begin() as conn:
+        await conn.run_sync(tables.Base.metadata.drop_all)
 
 
 @pytest.fixture(autouse=True)
-def migrate_table_schemas(stub_sess_manager: SessionManager):
-    from app.db.tables import Base
+async def migrate_table_schemas(stub_sess_manager: SessionManager):
 
-    sync_engine = create_engine(
-        stub_sess_manager.engine.url.set(drivername="postgresql")
-    )
-    Base.metadata.create_all(bind=sync_engine)
+    async with stub_sess_manager.engine.begin() as conn:
+        await conn.run_sync(tables.Base.metadata.create_all)
 
     yield
 
-    Base.metadata.drop_all(bind=sync_engine)
-    sync_engine.dispose()
+    async with stub_sess_manager.engine.begin() as conn:
+        await conn.run_sync(tables.Base.metadata.drop_all)
 
 
 @pytest.fixture
@@ -74,39 +83,32 @@ async def test_session(stub_sess_manager) -> AsyncGenerator[AsyncSession, None]:
 
 
 @pytest.fixture
-async def auth_redis() -> AsyncGenerator[Redis, None]:
-    settings = get_app_settings()
-    auth_redis = Redis.from_url(
-        settings.auth_redis_url, decode_responses=True, socket_timeout=1
-    )
-    yield auth_redis
-    await auth_redis.flushdb()
+async def auth_redis() -> AsyncGenerator[RedisMock, None]:
+    redis_mock = RedisMock()
+    yield redis_mock
+    await redis_mock.flushdb()
 
 
 @pytest.fixture
-async def app(test_session) -> FastAPI:
-    from app.depends.auth import get_user_db
+async def app(stub_sess_manager) -> FastAPI:
     from app.main import get_application
 
+    async def override_get_session():
+        async with stub_sess_manager.async_session() as session:
+            yield session
+
     app = get_application()
-    app.dependency_overrides[get_user_db] = test_session
-    return get_application()
+    app.dependency_overrides[get_session] = override_get_session
+    return app
 
 
 @pytest.fixture
 async def user_manager_stub(app, test_session: AsyncSession):
 
-    from fastapi_users import exceptions
-    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
-
-    from app.core.auth.manager import UserManager
-    from app.db.tables import User
-    from app.depends.auth import get_user_db, get_user_manager
-
     class StubUserManager(UserManager):
         async def authenticate(
             self, credentials: OAuth2PasswordRequestForm
-        ) -> User | None:
+        ) -> tables.User | None:
             try:
                 user = await self.get_by_email(credentials.username)
             except exceptions.UserNotExists:
@@ -118,5 +120,5 @@ async def user_manager_stub(app, test_session: AsyncSession):
 
     app.dependency_overrides[get_user_manager] = get_test_user_manager
 
-    test_user_db = SQLAlchemyUserDatabase(test_session, User, None)
+    test_user_db = SQLAlchemyUserDatabase(test_session, tables.User, None)
     return StubUserManager(user_db=test_user_db)
