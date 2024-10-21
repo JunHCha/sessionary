14abc10d7a356ffb2dfb2a583be8b8f544645e6f
junhcha <quffl9612@naver.com>
2024-10-21 19:25:05 +0900

    style: apply default font

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -46,6 +46,12 @@
 </main>
 
 <style>
+	:global(body) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+			'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+			'Noto Color Emoji';
+	}
+
 	main {
 		padding: 1rem;
 	}
