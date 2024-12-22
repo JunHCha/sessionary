a594dcacddd1ad3713f329da21b2bb99fea5bc96
junhcha <quffl9612@naver.com>
2024-12-22 19:04:10 +0900

    test: add notfound redirect test

--- frontend/tests/routes/home.test.ts
+++ frontend/tests/routes/home.test.ts
@@ -40,3 +40,10 @@ test.describe('Home page contents', () => {
 		await expect(newSection).toBeVisible()
 	})
 })
+
+test.describe('404 page', () => {
+	test('should display 404 page', async ({ page }) => {
+		await page.goto('/something-invalid-route')
+		await expect(page.locator('h1')).toHaveText('404')
+	})
+})
