73db02a3f2aa1713e6f452d675efd6b91aa0fc43
junhcha <quffl9612@naver.com>
2024-12-22 18:39:46 +0900

    fix: move logo path

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -6,7 +6,7 @@
 
 <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b ">
 	<NavBrand href="/home">
-		<img src="/src/assets/logo.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+		<img src="/images/logo.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
 			>Sessionary</span
 		>
