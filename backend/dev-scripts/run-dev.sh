e9366a8ac4cf30e689e4f15b05d75689d73fe35b
junhcha <quffl9612@naver.com>
2025-10-12 03:12:41 +0900

    chore: update Docker configuration and scripts for development environment

--- backend/dev-scripts/run-dev.sh
+++ backend/dev-scripts/run-dev.sh
@@ -1,11 +1,9 @@
 set +ex
 
-export APP_ENV=dev
-export DATABASE_URL=postgresql+asyncpg://user:password@localhost:5432/sessionary
-
+env
 docker compose up -d
 
-alembic upgrade head
-uvicorn --host 0.0.0.0 --port 8000 app.main:app --reload
+uv run alembic upgrade head
+uv run uvicorn --host 0.0.0.0 --port 8000 app.main:app --reload
 
 docker compose down
