c7cab18f2e0af405b746af3dfcb9bdac8becedf2
junhcha <quffl9612@naver.com>
2025-10-12 02:36:21 +0900

    refactor: update Dockerfile staging script

--- backend/dev-scripts/run-staging.sh
+++ backend/dev-scripts/run-staging.sh
@@ -1,5 +1,5 @@
 set +ex
 
 env
-alembic upgrade head
-uvicorn --host 0.0.0.0 app.main:app --port 10080 --workers 3
+uv run alembic upgrade head
+uv run uvicorn --host 0.0.0.0 app.main:app --port 10080 --workers 3
