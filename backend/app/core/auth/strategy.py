f5bdd03dba6da4b9b156982324bf696742057cb0
junhcha <quffl9612@naver.com>
2025-12-01 20:49:06 +0900

    refactor: integrate RedisMock class into auth strategy and remove redundant mock file

--- backend/app/core/auth/strategy.py
+++ backend/app/core/auth/strategy.py
@@ -1,4 +1,5 @@
 import secrets
+import time
 from typing import Optional
 
 import orjson
@@ -8,7 +9,57 @@ from fastapi_users.authentication.strategy import RedisStrategy
 from fastapi_users.manager import BaseUserManager
 
 from app.user.models import AuthSessionSchema, Subscription
-from tests.mock.redis_mock import RedisMock
+
+
+class RedisMock:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.data = {}
+            cls._instance.expires = {}
+        return cls._instance
+
+    async def set(self, key, value, *args, **kwargs):
+        self.data[key] = value
+        if "ex" in kwargs:
+            self.expires[key] = time.time() + kwargs["ex"]
+
+    async def setex(self, key, seconds, value):
+        self.data[key] = value
+        self.expires[key] = time.time() + seconds
+
+    async def get(self, key):
+        if key in self.expires:
+            if time.time() > self.expires[key]:
+                del self.data[key]
+                del self.expires[key]
+                return None
+        return self.data.get(key)
+
+    async def ttl(self, key):
+        if key not in self.expires:
+            return -1
+        remaining = int(self.expires[key] - time.time())
+        if remaining <= 0:
+            del self.data[key]
+            del self.expires[key]
+            return -2
+        return remaining
+
+    async def flushdb(self):
+        self.data.clear()
+        self.expires.clear()
+
+    async def delete(self, key: str) -> int:
+        existed = 0
+        if key in self.data:
+            del self.data[key]
+            existed = 1
+        if key in self.expires:
+            del self.expires[key]
+        return existed
 
 
 class CustomRedisStrategy(RedisStrategy):
